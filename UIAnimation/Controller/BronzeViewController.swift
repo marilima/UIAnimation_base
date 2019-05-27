e859b2bbfbe0d73a93a46abeb77a66037edcba69
Lucas Fernandez Nicolau <dev.lucasfn@icloud.com>
2019-05-27 17:48:51 -0300

    Arquivos adicionados ao versionamento e desafios bronze e prata finalizados

--- UIAnimation/Controller/BronzeViewController.swift
+++ UIAnimation/Controller/BronzeViewController.swift
@@ -8,23 +8,87 @@
 
 import UIKit
 
+func delay(_ seconds: Double, completion: @escaping ()->Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+}
+
 class BronzeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var world: UIImageView!
+    
+    let backgroundColors = [#colorLiteral(red: 0.2039215686, green: 0.2862745098, blue: 0.368627451, alpha: 1), #colorLiteral(red: 0.5333333333, green: 0.7529411765, blue: 0.968627451, alpha: 1)]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.world.frame.size.width = self.view.frame.size.width
+        self.world.center.x = self.view.center.x
+        self.world.center.y = self.view.frame.height
+        
+        self.rocket.center.x = self.view.center.x
+        self.rocket.center.y = self.world.center.y - self.world.frame.height / 2
+        self.rocket.frame.size.height = self.view.frame.size.height * 0.30
+        self.rocket.layer.zPosition = 1000
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 5.0, delay: 0, options: [.curveLinear], animations: {
+            self.rocket.image = #imageLiteral(resourceName: "rocket")
+            self.rocket.center.y = -self.rocket.frame.height
+        })
+        
+        delay(1.5) {
+            self.createSegmentedAnimation(from: CGPoint(x: self.view.center.x, y: self.view.center.y), to: CGPoint(x: self.view.center.x, y: -100), withDuration: 5.0)
+        }
+        
+        delay(2.0) {
+            self.createSegmentedAnimation(from: CGPoint(x: self.view.center.x - 40, y: self.view.center.y), to: CGPoint(x: self.view.center.x - 40, y: -100), withDuration: 3.0)
+            
+            self.createSegmentedAnimation(from: CGPoint(x: self.view.center.x + 40, y: self.view.center.y), to: CGPoint(x: self.view.center.x + 40, y: -100), withDuration: 3.0)
+        }
+    }
+    
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        self.view.backgroundColor = backgroundColors[sender.selectedSegmentIndex]
+    }
+    
+    func createSegmentedAnimation(from start: CGPoint, to end: CGPoint, withDuration duration: TimeInterval) {
+        let myPath = UIBezierPath()
+        myPath.move(to: start)
+        myPath.addLine(to: end)
+        
+        let myShapeLayer = CAShapeLayer()
+        myShapeLayer.strokeColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        myShapeLayer.lineWidth = 2
+        myShapeLayer.path = myPath.cgPath
+        myShapeLayer.strokeStart = 0.8
+        
+        // Animacão para desenhar a linha
+        let myEndAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        myEndAnimation.fromValue = 0.2
+        myEndAnimation.toValue = 1.0
+        
+        // Animação para apagar a linha
+        let myStartAnimation = CABasicAnimation(keyPath: "strokeStart")
+        myStartAnimation.fromValue = 0.0
+        myStartAnimation.toValue = 0.8
+        
+        let animationGroup = CAAnimationGroup()
+        animationGroup.animations = [myStartAnimation, myEndAnimation]
+        animationGroup.duration = duration
+        
+        myShapeLayer.add(animationGroup, forKey: "drawLine")
+        self.view.layer.addSublayer(myShapeLayer)
+    }
 }
--- UIAnimation/Controller/PrataViewController.swift
+++ UIAnimation/Controller/PrataViewController.swift
@@ -10,21 +10,91 @@ import UIKit
 
 class PrataViewController: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var heading: UILabel!
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var world: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 8.0
+        loginButton.layer.masksToBounds = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        heading.center.x = -view.bounds.width
+        username.center.x = -view.bounds.width
+        password.center.x = -view.bounds.width
+        loginButton.center.x = -view.bounds.width
+        
+        self.world.frame.size.width = self.view.frame.size.width
+        self.world.center.x = self.view.center.x
+        self.world.center.y = self.view.frame.height
+        
+        self.rocket.center.x = self.view.center.x
+        self.rocket.center.y = -self.rocket.frame.height
+        self.rocket.frame.size.height = self.view.frame.size.height * 0.30
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 2.35, delay: 0, options: [.curveEaseOut], animations: {
+            self.rocket.center.y = self.world.center.y - self.world.frame.height / 2
+        }, completion: { finished in
+            self.rocket.image = #imageLiteral(resourceName: "rocket2")
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseInOut], animations: {
+                self.heading.center.x = self.view.center.x
+            }, completion: nil)
 
+            UIView.animate(withDuration: 0.5, delay: 0.3, options: [.curveEaseInOut], animations: {
+                self.username.center.x = self.view.center.x
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 0.5, delay: 0.4, options: [.curveEaseInOut], animations: {
+                self.password.center.x = self.view.center.x
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 0.5, delay: 0.5, options: [.curveEaseInOut], animations: {
+                self.loginButton.center.x = self.view.center.x
+            }, completion: nil)
+        })
+    }
+
+    @IBAction func login() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseInOut], animations: {
+            self.heading.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: [.curveEaseInOut], animations: {
+            self.username.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.4, options: [.curveEaseInOut], animations: {
+            self.password.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [.curveEaseInOut], animations: {
+            self.loginButton.center.x += self.view.bounds.width
+        }, completion: { completed in
+            
+            UIView.animate(withDuration: 2.35, delay: 0, options: [.curveEaseOut], animations: {
+                self.rocket.image = #imageLiteral(resourceName: "rocket")
+                self.rocket.center.y = -self.rocket.frame.height / 2
+            }, completion: { completed in
+                self.performSegue(withIdentifier: "loggedIn", sender: self)
+            })
+            
+        })
+    }
 }
